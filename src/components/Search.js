ec31374ec24e7fa178a1c71aede473a1947326f0
Ashley Mojica <ashjmojica@gmail.com>
2023-02-07 16:26:31 -0600

    done

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,12 +1,15 @@
 import React from "react";
 
-function Search({handleChange}) {
+function Search({handleChange, searchTerm}) {
+  function handleChange(event) {
+    onChange(event.target.value);
+  }
 
   
   return (
     <div className="ui search">
       <div className="ui icon input">
-        <input onChange={handleChange} className="prompt" />
+        <input onChange={handleChange} value={searchTerm} className="prompt" />
         <i className="search icon" />
       </div>
     </div>
